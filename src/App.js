39af7919eb82f39c33aa1371f2cbee290c8af747
vickyganesan <g.arumugaperumal@yahoo.com>
2024-03-31 18:10:42 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
 
 function App() {
+  const siteName = "Thithippu Stores"
+  const siteName1 = <h1>Thithippu Super Stores</h1>
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>{siteName}</h3>
+      {siteName1}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
